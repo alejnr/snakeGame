1a87be53c55b2386632b0d1511eb5709bd51e7dc
Alex Menlah <alexamenlah@gmail.com>
2025-01-16 02:06:19 +0000

    creating the snake body

--- main.py
+++ main.py
@@ -6,10 +6,23 @@ screen.setup(width=600, height=600)
 screen.bgcolor('black')
 screen.title("Snake Game")
 
+starting_position = [(0,0), (-20,0), (-40,0)]
 
+for pos in starting_position:
+    new_segment = Turtle(shape='square')
+    new_segment.color('white')
+    new_segment.goto(pos)
 
-
-
+# turtle1 = Turtle("square")
+# turtle1.color("white")
+#
+# turtle2 = Turtle("square")
+# turtle2.color("white")
+# turtle2.goto(-20, 0)
+#
+# turtle3 = Turtle("square")
+# turtle3.color("white")
+# turtle3.goto(-40, 0)
 
 
 
